35447198207fcc35121c39ac918632ce6bcc24b9
Sjors Wijsman <sjorswijsman@gmail.com>
2023-06-09 00:28:49 +0200

    added pico for basic styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,29 @@
-<a href="/">News</a>
-<a href="/share">Share</a>
-<a href="/user">User</a>
-<slot />
+<script>
+	import '@picocss/pico/css/pico.min.css';
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li><a href="/">News</a></li>
+			<li><a href="/share">Share</a></li>
+			<li><a href="/user">User</a></li>
+		</ul>
+	</nav>
+</header>
+<main>
+	<slot />
+</main>
+
+<style>
+	header,
+	main {
+		padding: 1rem 2rem;
+		max-width: 30rem;
+		margin: 0 auto;
+	}
+
+	main {
+		padding-bottom: 20rem;
+	}
+</style>
